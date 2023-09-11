ea7050cec6a1f15b9048be9a0c1832f27e2f2a6b
Atonix0 <116169309+Atonix0@users.noreply.github.com>
2023-09-11 22:04:51 +0000

    spilt tokenizer and main .cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,89 +4,10 @@
 #include <numeric>
 #include <sstream>
 #include <fstream>
+#include "./tokenizer.cpp"
+
 using namespace std;
 
-enum TokenType {
-  Right_Pointer,
-  Left_Pointer,
-  Add_Value,
-  Remove_Value,
-  Open_Loop,
-  Close_Loop,
-  ERR,
-  END
-};
-
-struct Token {
-  TokenType type;
-
-  //for error checking
-  int colmun;
-  int line;
-};
-
-class Tokenizer {
-  public:
-  vector<Token> Tokens;
-  string code;
-  int line = 1;
-  int colmun = 1;
-  Tokenizer(string codes) {
-    code = codes;
-  }
-  void take() { if(code.length() > 0) code = code.substr(1); colmun++; }
-  void add(TokenType type) {
-    take();
-    Token toAdd;
-    toAdd.line = line;
-    toAdd.colmun = colmun;
-    toAdd.type = type;
-    
-    Tokens.push_back(toAdd);
-  }
-
-  vector<Token> tokenize() {
-    while(code.length() > 0) {
-      switch(code[0]) {
-        case '>': 
-          add(TokenType::Right_Pointer);
-          continue;
-        case '<':
-          add(TokenType::Left_Pointer);
-          continue;
-        case '[':
-          add(TokenType::Open_Loop);
-          continue;
-        case ']':
-          add(TokenType::Close_Loop);
-          continue;
-        case '+':
-          add(TokenType::Add_Value);
-          continue;
-        case '-':
-          add(TokenType::Remove_Value);
-          continue;
-        case ' ':
-          take();
-          continue;
-        case '\t': 
-          take();
-          continue;
-        case '\n':
-          take();
-          line++;
-          colmun = 0;
-          continue;
-        default: 
-          cout << "\033[1;31merror unknown char '\033[0m" << code[0] << "\033[1;31m'\n at => line:\033[0m" << line << "\033[1;31m, colmun:\033[0m"<< colmun << endl;
-          add(TokenType::ERR);
-          break;
-      }
-    }
-    add(TokenType::END);
-    return Tokens;
-  }
-};
 
 int main() {
   cout << "enter file path: ";
